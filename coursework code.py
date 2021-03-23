e7258a3263f9f8da65b3c15bb59623aa495a1bc7
chxque <31823199+chxque@users.noreply.github.com>
2021-03-23 12:36:34 +0000

    Update coursework code.py

--- coursework code.py
+++ coursework code.py
@@ -1,5 +1,16 @@
 n = 26
+import argparse
 
+def get_args():
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--e', action='store_true')
+    parser.add_argument('--d', action='store_true')  
+    parser.add_argument('-k','-key')
+    parser.add_argument('-m','-message',default='', nargs='+')
+    args = parser.parse_args()
+    args.m = ' '.join(args.m)
+    return args.k, args.e, args.d, args.m
+    
 def eEuclidean(a):
     x,y,previousX,previousY = 1,a,0,n
     while (y):
@@ -41,5 +52,15 @@ def parse(a,b,text,execute):
     return transformedtext
     
 
-print(decrypt(5,8,'SIV OC MCZ I FAMSRIQF WV SRIZ?'))
-print(encrypt(5,8,'CAN WE GET A POGCHAMP IN CHAT?'))
+
+key, encryptBool, decryptBool, messageText = get_args()
+a,b=[int(x) for x in key.split(',')]
+
+if(encryptBool):
+    print(encrypt(a,b,messageText))
+
+if(decryptBool):
+    print(decrypt(a,b,messageText))
+#print(decrypt(5,8,'SIV OC MCZ I FAMSRIQF WV SRIZ?'))
+#print(encrypt(5,8,'CAN WE GET A POGCHAMP IN CHAT?'))
+
